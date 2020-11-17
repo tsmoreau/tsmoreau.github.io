a1eb11f3939a38090f3c32f84bbd9f27c3b81c5a
tsmoreau <64439028+tsmoreau@users.noreply.github.com>
2020-11-17 14:53:13 -0800

    Update app.js

--- app.js
+++ app.js
@@ -85,6 +85,8 @@ async function fetchAccountData() {
   // Get connected chain id from Ethereum node
   const chainId = await web3.eth.getChainId();
   // Load chain information over an HTTP API
+  const chainData = evmChains.getChain(chainId);
+  document.querySelector("#network-name").textContent = chainData.name;
 
   // Get list of accounts of the connected wallet
   const accounts = await web3.eth.getAccounts();
