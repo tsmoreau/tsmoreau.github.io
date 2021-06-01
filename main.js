4712cd5df36819c00770ef0a288a95d5b14df748
tsmoreau <64439028+tsmoreau@users.noreply.github.com>
2021-06-01 10:35:00 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-Moralis.initialize("HJKwn5nm8X1ZUanGa6ZoLgnJyoG4KUgKw4Y7362L");
-Moralis.serverURL = 'https://dajbzubkfnoi.moralis.io:2053/server';
-const TOKEN_CONTRACT_ADDRESS = "0x1192b542d1A32eA9C7a1B774314849CcE2684144";
-const MARKETPLACE_CONTRACT_ADDRESS = "0x1d6A4CF64B52F6c73f201839AdED7379Ce58059c";
+Moralis.initialize("njzKCZK2k5pOg36fxFMaGfyCpCyJ6paWTAiJkNqt");
+Moralis.serverURL = 'https://zy7ycvilboxy.moralis.io:2053/server';
+const TOKEN_CONTRACT_ADDRESS = "0x110eE108f45330E3Ae7e538614AA4eBD525E6A9D";
+const MARKETPLACE_CONTRACT_ADDRESS = "0xC410AcefF25B05DEA7aDB65c7B92Bc272D9b87F3";
 
 
 init = async () => {
